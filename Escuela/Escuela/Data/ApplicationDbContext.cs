fbb5238a357d38f6968f4e4c9eb86819baa76d0f
davidcalles1 <cc0324032019@unab.edu.sv>
2021-09-24 14:51:53 -0600

    1

--- Escuela/Escuela/Data/ApplicationDbContext.cs
+++ Escuela/Escuela/Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Escuela.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
     }
 }
--- Escuela/Escuela/Startup.cs
+++ Escuela/Escuela/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Escuela.Data;
 
 namespace Escuela
 {
@@ -24,6 +26,9 @@ namespace Escuela
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(optios =>
+            optios.UseSqlServer(Configuration.GetConnectionString
+            ("DefaulConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
